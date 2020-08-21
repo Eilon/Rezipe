aa500be88297be8c3eb447e623f5a510f81c5866
Eilon Lipton <Eilon@users.noreply.github.com>
2020-08-21 14:41:53 -0700

    Add spaghetti squash burrito recipe

--- src/Rezipe/RecipeStore.cs
+++ src/Rezipe/RecipeStore.cs
@@ -37,6 +37,50 @@ namespace Rezipe
                     "Close lid and cook until waffle is golden brown but still steaming.",
                 },
             },
+
+            new Recipe
+            {
+                ID = 2,
+                Title= "Spaghetti squash burrito",
+                Servings = 4,
+                CookTime = new TimeSpan(hours: 1, minutes: 0, seconds: 0),
+                Ingredients = new List<Ingredient>
+                {
+                    new Ingredient{ Name="medium spaghetti squash, halved, seeds removed", Quantity=2m, Unit="", },
+                    new Ingredient{ Name="extra-virgin olive oil", Quantity=2m, Unit="tbsp", },
+                    new Ingredient{ Name="Kosher salt", Quantity=2m, Unit="pinch", },
+                    new Ingredient{ Name="chili powder", Quantity=0.5m, Unit="tsp", },
+                    new Ingredient{ Name="cumin", Quantity=0.5m, Unit="tsp", },
+                    new Ingredient{ Name="Onion", Quantity=1m, Unit="", },
+                    new Ingredient{ Name="Garlic, minced", Quantity=2m, Unit="cloves", },
+                    new Ingredient{ Name="Ground beef", Quantity=1m, Unit="lbs", },
+                    new Ingredient{ Name="Taco seasoning mix", Quantity=1m, Unit="tbsp", },
+                    new Ingredient{ Name="Freshly ground black pepper", Quantity=1m, Unit="", },
+                    new Ingredient{ Name="Can of black beans", Quantity=15m, Unit="oz", },
+                    new Ingredient{ Name="Chopped cherry tomatoes", Quantity=1.5m, Unit="cup", },
+                    new Ingredient{ Name="Can of corn, drained", Quantity=1m, Unit="cup", },
+                    new Ingredient{ Name="Shredded Monterey Jack cheese", Quantity=1m, Unit="cup", },
+                    new Ingredient{ Name="Shredded cheddar cheese", Quantity=0.5m, Unit="cup", },
+                },
+                Steps = new List<string>
+                {
+                    "Preheat oven to 400°F.",
+                    "Squash: Drizzle cut sides of spaghetti squash with oil and season with salt, chili powder, and cumin.",
+                    "Place cut side down on a large, rimmed baking sheet.",
+                    "Roast until tender, 30 to 35 minutes.",
+                    "Let cool slightly.",
+                    "Using a fork, break up squash strands.",
+                    "Filling: In a large skillet over medium heat, heat olive oil.",
+                    "Add onion and cook until soft, about 5 minutes.",
+                    "Stir in garlic and cook until fragrant, about 1 minute more.",
+                    "Add ground beef until no longer pink, about 6 minutes.",
+                    "Drain fat.",
+                    "Stir in taco seasoning, then season to taste with salt and pepper.",
+                    "Stir in black beans, cherry tomatoes, and corn.",
+                    "Fill each spaghetti squash with beef mixture and top with cheeses.",
+                    "Return to oven to melt cheese, 5 minutes.",
+                },
+            },
         };
 
         public List<Recipe> GetRecipes()
