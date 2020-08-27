6231161567b0fb579823f7cf5e1442bdcbbebbd5
Eilon Lipton <Eilon@users.noreply.github.com>
2020-08-27 16:35:12 -0700

    Move ingredient logic to a ViewModel
    - The view delegates most logic to the ViewModel
    - Added ability to tape original servings to reset servings to that value
    - Updated MobileBlazorBindings version to get a bug fix for sliders

---
+++ src/Rezipe/ViewModels/IngredientListing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Rezipe.ViewModels
+{
+    public class IngredientListing
+    {
+        public IngredientListing(RecipeDisplay recipeDisplay, Ingredient ingredient)
+        {
+            if (ingredient is null)
+            {
+                throw new ArgumentNullException(nameof(ingredient));
+            }
+            RecipeDisplay = recipeDisplay;
+            Ingredient = ingredient;
+        }
+
+        private RecipeDisplay RecipeDisplay { get; }
+        private Ingredient Ingredient { get; }
+
+        public bool IsDone { get; set; }
+
+        public string Name => Ingredient.Name;
+        public decimal Quantity => Ingredient.Quantity * RecipeDisplay.ScaledServings / RecipeDisplay.OriginalServings;
+        public string Unit => Ingredient.Unit;
+    }
+}
---
+++ src/Rezipe/ViewModels/RecipeDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rezipe.ViewModels
+{
+    public class RecipeDisplay
+    {
+        public RecipeDisplay(Recipe recipe)
+        {
+            Recipe = recipe ?? throw new ArgumentNullException(nameof(recipe));
+
+            IngredientListings = Recipe.Ingredients.Select(i => new IngredientListing(this, i)).ToList();
+            ScaledServings = OriginalServings;
+        }
+
+        private Recipe Recipe { get; }
+
+        public string Title => Recipe.Title;
+        public List<IngredientListing> IngredientListings { get; }
+        public int OriginalServings => Recipe.Servings;
+        public int ScaledServings { get; set; }
+        public TimeSpan CookTime => Recipe.CookTime;
+        public List<string> Steps => Recipe.Steps;
+    }
+}
