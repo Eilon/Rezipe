6f290fed160b0266e3735ae7db6ad3ce86028f9d
Eilon Lipton <Eilon@users.noreply.github.com>
2020-09-04 11:17:03 -0700

    Set Shell flyout color to work in dark and light mode

---
+++ src/Rezipe/RezipeApp.razor.cs
@@ -0,0 +1,60 @@
+﻿using Xamarin.Forms;
+
+namespace Rezipe
+{
+    public partial class RezipeApp
+    {
+        private DataTemplate GetShellItemTemplate()
+        {
+            return new DataTemplate(() =>
+            {
+                var grid = new Grid()
+                {
+                    ColumnDefinitions = new ColumnDefinitionCollection
+                    {
+                        new ColumnDefinition{ Width = 50, },
+                        new ColumnDefinition{ Width = GridLength.Star, },
+                    },
+                };
+
+                var itemImage = new Image
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Center,
+                    HeightRequest = 22,
+                    WidthRequest = 22,
+                };
+
+                var itemLabel = new Label
+                {
+                    TextColor = Color.Black,
+                    FontSize = 20d,
+                    FontAttributes = FontAttributes.Bold,
+                    VerticalTextAlignment = TextAlignment.Center,
+                };
+
+                itemImage.SetBinding(Image.SourceProperty, "FlyoutIcon");
+                itemLabel.SetBinding(Label.TextProperty, "Title");
+
+                grid.Children.Add(itemImage);
+                grid.Children.Add(itemLabel, 1, 0);
+
+                var normalState = new VisualState { Name = "Normal" };
+                normalState.Setters.Add(new Setter { Property = Grid.BackgroundColorProperty, Value = Color.DarkGray, });
+                var selectedState = new VisualState { Name = "Selected" };
+                selectedState.Setters.Add(new Setter { Property = Grid.BackgroundColorProperty, Value = Color.LightGray, });
+
+                var commonStateGroup = new VisualStateGroup { Name = "CommonStates" };
+                commonStateGroup.States.Add(normalState);
+                commonStateGroup.States.Add(selectedState);
+
+                VisualStateManager.SetVisualStateGroups(grid, new VisualStateGroupList
+                {
+                    commonStateGroup,
+                });
+
+                return grid;
+            });
+        }
+    }
+}
