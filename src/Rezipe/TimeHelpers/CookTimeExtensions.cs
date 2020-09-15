be4464b697368fe86c9ad17849e2fd0c8ed27915
Eilon Lipton <Eilon@users.noreply.github.com>
2020-09-15 14:38:16 -0700

    Show cook time in recipe detail

---
+++ src/Rezipe/TimeHelpers/CookTimeExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Rezipe.TimeHelpers
+{
+    public static class CookTimeExtensions
+    {
+        public static string ToCookTimeDisplay(this TimeSpan cookTime)
+        {
+            var ret = new StringBuilder();
+            if (cookTime.Days > 0)
+            {
+                ret.AppendFormat(CultureInfo.CurrentCulture, "{0}d", cookTime.Days);
+            }
+            if (cookTime.Hours > 0)
+            {
+                if (ret.Length > 0)
+                {
+                    ret.Append(" ");
+                }
+                ret.AppendFormat(CultureInfo.CurrentCulture, "{0}h", cookTime.Hours);
+            }
+            if (cookTime.Minutes > 0)
+            {
+                if (ret.Length > 0)
+                {
+                    ret.Append(" ");
+                }
+                ret.AppendFormat(CultureInfo.CurrentCulture, "{0}m", cookTime.Minutes);
+            }
+            // Ignore seconds. Recipes aren't specified in seconds.
+            if (ret.Length == 0)
+            {
+                return "unknown";
+            }
+            return ret.ToString();
+        }
+    }
+}
