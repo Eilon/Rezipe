222d2c622750009e3212197ab9666351e269bb0c
Eilon Lipton <Eilon@users.noreply.github.com>
2020-10-16 00:36:50 -0700

    Enable dark/light mode
    - Bump version to 1.3
    - Add DebugPage in debug builds
    
    Fixes #1

--- src/Rezipe/RezipeApp.razor.cs
+++ src/Rezipe/RezipeApp.razor.cs
@@ -1,10 +1,39 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace Rezipe
 {
     public partial class RezipeApp
     {
-        private DataTemplate GetShellItemTemplate()
+#if DEBUG
+        public static bool DebugMode => true;
+#else
+        public static bool DebugMode => false;
+#endif
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            var theme = Application.Current.RequestedTheme;
+            DebugPage.WriteLine($"Current theme: {theme}");
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                Shell.Current.ItemTemplate = GetShellItemTemplate();
+
+                // Set ShellItem to the container of the Favorite Recipes page
+                Shell.Current.CurrentItem = favRecipesPage.NativeControl;
+
+                // Set ShellContent within tabs to the actual Favorite Recipes page
+                Shell.Current.CurrentItem.CurrentItem = favRecipesPage.NativeControl;
+            }
+        }
+
+        private static DataTemplate GetShellItemTemplate()
         {
             return new DataTemplate(() =>
             {
@@ -27,11 +56,11 @@ namespace Rezipe
 
                 var itemLabel = new Label
                 {
-                    TextColor = Color.Black,
                     FontSize = 20d,
                     FontAttributes = FontAttributes.Bold,
                     VerticalTextAlignment = TextAlignment.Center,
                 };
+                itemLabel.SetAppThemeColor(Label.TextColorProperty, light: Color.Black, dark: Color.White);
 
                 itemImage.SetBinding(Image.SourceProperty, "FlyoutIcon");
                 itemLabel.SetBinding(Label.TextProperty, "Title");
---
+++ src/Rezipe/RezipeBaseComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using Xamarin.Forms;
+
+namespace Rezipe
+{
+    public class RezipeComponentBase : ComponentBase, IDisposable
+    {
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Application.Current.RequestedThemeChanged += OnRequestedThemeChanged;
+        }
+
+        private void OnRequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
+        {
+            DebugPage.WriteLine($"Theme changed on {GetType().Name} to: {e.RequestedTheme}");
+
+            // Re-render UI when app theme changes between dark/light
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            Application.Current.RequestedThemeChanged -= OnRequestedThemeChanged;
+        }
+
+        protected Color GetThemeColor(Color lightColor, Color darkColor)
+        {
+            return Application.Current.RequestedTheme == OSAppTheme.Dark ? darkColor : lightColor;
+        }
+    }
+}
